f8759544cd53bf9fbfc5424b25d618064d9b7e28
Rafael Franco <rafaelfr@outlook.com>
2022-01-07 16:45:42 -0300

    feat: initial files

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`
+
+const Title = styled.h1`
+  color: #1e31e3;
+`
+
+const Index = () => {
+  return (
+    <Container>
+      <Title>rfoel.dev</Title>
+    </Container>
+  )
+}
+
+export default Index
