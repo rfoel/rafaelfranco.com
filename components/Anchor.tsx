59bb7d66202cd7e51ae53ef4c9eb2d2b49c2b508
Rafael Franco <rafaelfr@outlook.com>
2022-01-12 20:33:16 -0300

    fix: remove background image (#9)

--- components/Anchor.tsx
+++ components/Anchor.tsx
@@ -2,11 +2,16 @@ import styled, { css } from 'styled-components'
 
 const Anchor = styled.a<{ active?: boolean }>(
   ({ active }) => css`
+    color: var(--blue);
     cursor: pointer;
     text-decoration: none;
 
     :visited {
-      color: initial;
+      color: var(--blue);
+    }
+
+    :hover {
+      font-weight: 700;
     }
 
     ${active &&
