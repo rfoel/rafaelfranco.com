8e0fd1cf893b0e9a1bc2470451a65f24b634604d
Rafael Franco <rafaelfr@outlook.com>
2022-09-08 17:47:28 -0300

    chore: lint

--- app/root.tsx
+++ app/root.tsx
@@ -39,18 +39,15 @@ const Document = withEmotionCache(
     const serverStyleData = useContext(ServerStyleContext)
     const clientStyleData = useContext(ClientStyleContext)
 
-    // Only executed on client
     useEffect(() => {
-      // re-link sheet container
       emotionCache.sheet.container = document.head
-      // re-inject tags
       const tags = emotionCache.sheet.tags
       emotionCache.sheet.flush()
       tags.forEach(tag => {
         ;(emotionCache.sheet as any)._insertTag(tag)
       })
-      // reset cache to reapply global styles
       clientStyleData?.reset()
+      // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
