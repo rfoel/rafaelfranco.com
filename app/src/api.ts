d2843228d70b5f13890232f85d9cee07402f2cdf
Rafael Franco <oi@rafaelfranco.com>
2023-07-02 11:37:06 -0300

    fix: fix api url

--- app/src/api.ts
+++ app/src/api.ts
@@ -1,5 +1,5 @@
 const baseUrl = import.meta.env.PROD
-  ? 'https://api.mail.rafaelfranco.com'
+  ? 'https://api.rafaelfranco.com'
   : 'http://localhost:3000'
 
 export const rest = (uri: string, init: RequestInit) =>
